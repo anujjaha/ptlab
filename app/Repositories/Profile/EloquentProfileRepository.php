13d329cb8df9b7d4c1df980140fb58d3f04758cb
Anuj <laraveldev1.ebiztrait@gmail.com>
2026-09-09 17:12:42 +0530

    x

--- app/Repositories/Profile/EloquentProfileRepository.php
+++ app/Repositories/Profile/EloquentProfileRepository.php
@@ -439,7 +439,6 @@ class EloquentProfileRepository extends DbRepository
             $input = array_merge($input, ['user_id' => access()->user()->id]);
         }
 
-        dd($input);
         return $input;
     }
 
